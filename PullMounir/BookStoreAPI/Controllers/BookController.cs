3c9f770d8d49654b6e1d782756f87ab247d205ec
Kadem Garnier <kadem.garnier@gmail.com>
2023-12-11 17:08:32 +0100

    analyse code

---
+++ PullMounir/BookStoreAPI/Controllers/BookController.cs
@@ -0,0 +1,37 @@
+using System.Collections.Generic;
+using BookStoreAPI.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace BookStoreAPI.Controllers; // BookStoreAPI est l'espace de nom racine de mon projet 
+
+
+
+// Ceci est une annotation, elle permet de définir des métadonnées sur une classe
+// Dans ce contexte elle permet de définir que la classe BookController est un contrôleur d'API
+// On parle aussi de decorator / décorateur
+[ApiController]
+public class BookController : ControllerBase
+{
+
+    // C'est un méthode GET qui permet de récupérer les livres, j'en créer égakelent un nouveau que j'ajoute à ma liste et je la retourne
+    [HttpGet("books")]
+    public ActionResult<List<Book>> GetBooks()
+    {
+        // je créer une liste de livres
+        var books = new List<Book>
+        {
+            // je créer un nouveau livre et je l'ajoute à ma liste
+            new() { Id = 1, Title = "Le seigneur des anneaux", Author = "J.R.R Tolkien" }
+        };
+
+        return Ok(books);
+
+    }
+
+    // je créer ma route qui me permet de créer un nouveau livre
+    [HttpPost("books")]
+    public ActionResult<Book> CreateBook([FromBody] Book book)
+    {
+    }
+}
